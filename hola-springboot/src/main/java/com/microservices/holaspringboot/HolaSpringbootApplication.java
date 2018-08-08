6e4fc9b861b55e6d6152014891a435a9c430778e
Fabiano <fabianoyoschitaki@gmail.com>
2018-08-08 13:57:48 -0300

    organizing folders

---
+++ hola-springboot/src/main/java/com/microservices/holaspringboot/HolaSpringbootApplication.java
@@ -0,0 +1,16 @@
+package com.microservices.holaspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+import com.microservices.holaspringboot.controller.HolaRestController;
+
+@SpringBootApplication
+//@ComponentScan(basePackageClasses = HolaRestController.class)
+public class HolaSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HolaSpringbootApplication.class, args);
+	}
+}
---
+++ hola-springboot/src/main/java/com/microservices/holaspringboot/controller/HolaRestController.java
@@ -0,0 +1,46 @@
+package com.microservices.holaspringboot.controller;
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+@ConfigurationProperties(prefix="helloapp")
+
+public class HolaRestController {
+	
+	private String saying;
+	private Integer id;
+	
+	@RequestMapping(method=RequestMethod.GET, value="/hola", produces="text/plain")
+	public String hola() {
+		String hostname = null;
+		try {
+			hostname = InetAddress.getLocalHost().getHostAddress();
+		} catch (UnknownHostException e) {
+			hostname = "unknown";
+		}
+		return saying + "-" + id + ": " + hostname;
+	}
+
+	public String getSaying() {
+		return saying;
+	}
+
+	public void setSaying(String saying) {
+		this.saying = saying;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+}
