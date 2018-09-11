7e8777fe1fa42962eb2f16e3e907175096ab22df
Fabiano <fabianoyoschitaki@gmail.com>
2018-09-11 18:17:07 -0300

    cripto

--- crypter/src/main/java/br/com/portoseguro/crypter/controller/CryptUtil.java
+++ crypter/src/main/java/br/com/portoseguro/crypter/controller/CryptUtil.java
@@ -19,7 +19,6 @@ public class CryptUtil {
             cipher.init(Cipher.ENCRYPT_MODE, skeySpec, iv);
 
             byte[] encrypted = cipher.doFinal(value.getBytes());
-            System.out.println("encrypted string: " + Base64.encodeBase64String(encrypted));
 
             return Base64.encodeBase64String(encrypted);
         } catch (Exception ex) {
